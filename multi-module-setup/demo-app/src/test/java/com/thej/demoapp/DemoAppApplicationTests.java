1a10286511fa4e46bcddcff4c4f7476770a35fe2
ThejMal01 <thejani.mallawaarachchi@gmail.com>
2025-09-05 01:51:21 +0530

    multi-module-setup: initial services setup done

--- multi-module-setup/demo-app/src/test/java/com/thej/demoapp/DemoAppApplicationTests.java
+++ multi-module-setup/demo-app/src/test/java/com/thej/demoapp/DemoAppApplicationTests.java
@@ -1,13 +1,13 @@
-package com.thej.serviceregistry;
+package com.thej.demoapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServiceRegistryApplicationTests {
+class DemoAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
--- multi-module-setup/service-registry/src/main/java/com/thej/dockerdemo/serviceregistry/ServiceRegistryApplication.java
+++ multi-module-setup/service-registry/src/main/java/com/thej/dockerdemo/serviceregistry/ServiceRegistryApplication.java
@@ -1,4 +1,4 @@
-package com.thej.serviceregistry;
+package com.thej.dockerdemo.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
