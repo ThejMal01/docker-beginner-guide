639249fd73a6a1c489af22d00e8fd066004aa3f0
ThejMal01 <thejani.mallawaarachchi@gmail.com>
2025-09-05 00:17:06 +0530

    Initial commit: docker-setup-with-mysql-for-single-app

---
+++ single-app-setup/docker-guide/src/test/java/com/thej/dockerguide/DockerGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thej.dockerguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
